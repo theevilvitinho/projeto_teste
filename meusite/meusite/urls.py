2a468204c5a3d1eeb62d3942a5886ec8f397a7f1
vitin <vitorborgesdeoliveira2308@gmail.com>
2026-07-06 20:35:13 -0300

    new project_teste

---
+++ meusite/meusite/urls.py
@@ -0,0 +1,28 @@
+"""
+URL configuration for meusite project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from . view import teste_view
+from . view import index_view
+from . view import administracao_view
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('teste/', teste_view),
+    path('index/', index_view),
+    path('administracao/', administracao_view)
+]
---
+++ meusite/meusite/view.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+
+def teste_view(request):
+    return HttpResponse("<h1>Olá mundo! Esta é uma view de teste.</h1>")
+
+def index_view(request):
+    return HttpResponse("<h1>Bem-vindo à página inicial!</h1><p style='color: blue'>Este é um novo projeto</p>")
+
+def administracao_view(request):
+    return HttpResponse("Admin teste")
